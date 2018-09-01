e033ea00e115524912b71a6246fb93c6af8daf07
Emilio Basualdo <Pilobas97@gmail.com>
2018-09-01 16:19:56 -0300

    asdf

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk
+DIR=`dirname $0`
+qemu-system-x86_64 -hda $DIR/Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk
